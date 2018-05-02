e6da8e98a667906a8bc32ae8a2fd1653e626c1c9
Emmanuel Anderson <manu1526@gmail.com>
2018-05-02 13:36:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens=["Milo","Otis","Garfield"]
 function destructivelyAppendKitten(kittens, Ralph){
-  kittens.push('Ralph');
+  kittens.unshift('Ralph');
     return (kittens);
 }
 

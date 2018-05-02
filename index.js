c2dc981673cfe1ab1bfc1b24e4aa9b8f68c55130
Emmanuel Anderson <manu1526@gmail.com>
2018-05-02 14:00:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function destructivelyPrependKitten(Bob){
 
 var kittens=["Milo","Otis","Garfield"]
 function destructivelyRemoveLastKitten(kittens){
-  kittens.pop();
+  kittens=kittens.slice(1);
     return (kittens);
 }
 

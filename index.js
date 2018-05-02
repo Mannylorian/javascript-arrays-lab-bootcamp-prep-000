66478d4fe71c581fc2a40a6737f079a8318158a8
Emmanuel Anderson <manu1526@gmail.com>
2018-05-02 13:31:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ const app = "I don't do much."
 var kittens=["Milo","Otis","Garfield"]
 function destructivelyAppendKitten(kittens, Ralph){
   kittens.push('Ralph');
-  return (kittens);
+    return (kittens);
 }
 
 var kittens=["Milo","Otis","Garfield"]
 function destructivelyAppendKitten(kittens, Ralph){
   kittens.unshift('Ralph');
-  return (kittens);
+    return (kittens);
 }
 
 var kittens=["Milo","Otis","Garfield"]
 function destructivelyRemoveLastKitten(kittens){
-  kittens.pop();
-  return (kittens);
+  kittens.pop(kittens);
+    return (kittens);
 }
 
 var kittens=["Milo","Otis","Garfield"]
 function destructivelyRemoveFirstKitten(kittens){
-  
+kittens.shift(kittens);
+  return (kittens);
 }
